954e26e0fc05371f121ecb4467ddce9e1ccc3a0a
Cascavel <simao.ramaral@gmail.com>
2021-08-18 15:20:05 +0100

    Update find.js

--- commands/find.js
+++ commands/find.js
@@ -1,15 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+
 exports.run = async (client, message, args) => {
 
-	const game = args[0];
-	const file = args[1];
+	const file = args[0];
 
-	if(game != null) {
-		const fetchGameFile = await client.fetch(`http://192.168.1.220:8080/api/gta/${game}}/${file}`);
+	if(file != null) {
+		const fetchGameFile = await client.fetch(`http://192.168.1.220:8080/api/gta/sa/?name${file}`);
 		const gameFileData = await fetchGameFile.json();
-		message.channel.send(gameFileData.value);
+
+		const fileEmbed = new MessageEmbed()
+			.setColor('#f15bcb')
+			.setTitle(gameFileData[0].name + ' Information')
+			.setThumbnail(gameFileData[0].urlTXD)
+			.addFields(
+				{ name: 'DFF', value: gameFileData[0].urlDFF },
+				{ name: 'TXD', value: gameFileData[0].urlTXD },
+			);
+		message.channel.send({ embeds: [fileEmbed] });
 	}
 	else {
-		message.reply('Please supply a valid country in the format `++covid [country]`');
+		message.reply('Please supply a valid file in the format `++find [fileName]`');
 		return;
 	}
 };
