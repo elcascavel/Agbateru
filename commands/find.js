8354fe7bf00770c10abaffb007dd5f4b762eb2c3
Cascavel <simao.ramaral@gmail.com>
2021-08-18 17:09:28 +0100

    Updated file.js
    Added check to ensure file is in database and inform user if otherwise;
    Fixed the fetch URL;
    The bot now sends the files as attachments correctly.

--- commands/find.js
+++ commands/find.js
@@ -1,22 +1,20 @@
-const { MessageEmbed } = require('discord.js');
-
 exports.run = async (client, message, args) => {
 
 	const file = args[0];
 
 	if(file != null) {
-		const fetchGameFile = await client.fetch(`http://192.168.1.220:8080/api/gta/sa/?name${file}`);
+		const fetchGameFile = await client.fetch(`http://192.168.1.220:8080/api/gta/sa/?name=${file}`);
 		const gameFileData = await fetchGameFile.json();
-
-		const fileEmbed = new MessageEmbed()
-			.setColor('#f15bcb')
-			.setTitle(gameFileData[0].name + ' Information')
-			.setThumbnail(gameFileData[0].urlTXD)
-			.addFields(
-				{ name: 'DFF', value: gameFileData[0].urlDFF },
-				{ name: 'TXD', value: gameFileData[0].urlTXD },
-			);
-		message.channel.send({ embeds: [fileEmbed] });
+		if(!gameFileData[0]) {
+			return message.channel.send(`No results found for **${file}**.`);
+		}
+		message.channel.send(`Pulling files from database pertaining to **${file}**.`);
+		message.channel.send({
+			files: [
+				gameFileData[0].urlDFF,
+				gameFileData[0].urlTXD,
+			],
+		});
 	}
 	else {
 		message.reply('Please supply a valid file in the format `++find [fileName]`');
