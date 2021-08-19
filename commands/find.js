3e2ffe39cc1ef816e66092f89f73e0845af46091
Cascavel <simao.ramaral@gmail.com>
2021-08-19 16:04:28 +0100

    Update find.js

--- commands/find.js
+++ commands/find.js
@@ -1,20 +1,37 @@
 exports.run = async (client, message, args) => {
 
-	const file = args[0];
+	const fileType = args[0];
+	const file = args[1];
 
-	if(file != null) {
-		const fetchGameFile = await client.fetch(`http://192.168.1.220:8080/api/gta/sa/?name=${file}`);
+	if(fileType != 'dff' || fileType != 'txd' || fileType != 'img' && file != null) {
+		const fetchGameFile = await client.fetch(`http://192.168.1.220:8080/api/gta/sa/${fileType}/?name=${file}`);
 		const gameFileData = await fetchGameFile.json();
 		if(!gameFileData[0]) {
-			return message.channel.send(`No results found for **${file}**.`);
+			return message.channel.send(`No results found for ${fileType} **${file}**.`);
 		}
 		message.channel.send(`Pulling files from database pertaining to **${file}**.`);
-		message.channel.send({
-			files: [
-				gameFileData[0].urlDFF,
-				gameFileData[0].urlTXD,
-			],
-		});
+		if (fileType === 'dff') {
+			message.channel.send({
+				files: [
+					gameFileData[0].urlDFF,
+				],
+			});
+		}
+		else if (fileType === 'txd') {
+			message.channel.send({
+				files: [
+					gameFileData[0].urlTXD,
+				],
+			});
+		}
+
+		else if (fileType === 'img') {
+			message.channel.send({
+				files: [
+					gameFileData[0].urlIMG,
+				],
+			});
+		}
 	}
 	else {
 		message.reply('Please supply a valid file in the format `++find [fileName]`');
