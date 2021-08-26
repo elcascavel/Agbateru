5739f5264d1a6a14023d6b6f871fb66ce719479b
Cascavel <simao.ramaral@gmail.com>
2021-08-26 17:00:14 +0100

    removed chuck command

--- commands/chuck.js
+++
@@ -1,5 +0,0 @@
-exports.run = async (client, message) => {
-	const response = await client.fetch('https://api.chucknorris.io/jokes/random');
-	const fact = await response.json();
-	message.reply(fact.value);
-};
